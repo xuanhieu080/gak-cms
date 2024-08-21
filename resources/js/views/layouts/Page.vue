1307cf5944a5e8dc6f7fb88acec5486632939a46
dunghm97 <dunghoang.5397@gmail.com>
2024-08-22 01:24:07 +0700

    Update Dashboard + Sidebar

--- resources/js/views/layouts/Page.vue
+++ resources/js/views/layouts/Page.vue
@@ -23,9 +23,12 @@
                             </a-menu-item>
                             <a-menu-item key="2">
                                 <UserOutlined />
-                                Trang chủ
+                                Cài đặt đăng nhập
                             </a-menu-item>
-                            <a-menu-item key="3"  @click.stop.prevent="handleLogout">
+                            <a-menu-item
+                                key="3"
+                                @click.stop.prevent="handleLogout"
+                            >
                                 <LogoutOutlined />
                                 Đăng xuất
                             </a-menu-item>
@@ -55,17 +58,107 @@
                     theme="dark"
                     mode="inline"
                 >
-                    <a-menu-item key="1">
-                        <user-outlined />
-                        <span>Dashboard</span>
+                    <a-menu-item key="1" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <HomeOutlined />
+                        </div>
+                        <span>Trang chủ</span>
                     </a-menu-item>
-                    <a-menu-item key="2">
-                        <video-camera-outlined />
-                        <span>Child note 1</span>
+                    <a-menu-item key="2" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <ShopOutlined />
+                        </div>
+                        <span>Quản lý sản phẩm</span>
                     </a-menu-item>
-                    <a-menu-item key="3">
-                        <upload-outlined />
-                        <span>Child note 2</span>
+                    <a-menu-item key="3" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <BankOutlined />
+                        </div>
+                        <span>Quản lý kho hàng</span>
+                    </a-menu-item>
+                    <a-menu-item key="4" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <DeliveredProcedureOutlined />
+                        </div>
+                        <span>Đơn chuyển kho - Giao khách</span>
+                    </a-menu-item>
+                    <a-menu-item key="5" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <CloudDownloadOutlined />
+                        </div>
+                        <span>Nhập kho - Điều chuyển kho</span>
+                    </a-menu-item>
+                    <a-menu-item key="6" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <TeamOutlined />
+                        </div>
+                        <span>Quản lý danh sách nhân viên</span>
+                    </a-menu-item>
+                    <a-menu-item key="7" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <ToolOutlined />
+                        </div>
+                        <span>Cấu hình</span>
+                    </a-menu-item>
+                    <a-menu-item key="8" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <AppstoreOutlined />
+                        </div>
+                        <span>Phân quyền</span>
+                    </a-menu-item>
+                    <a-menu-item key="9" :class="collapsed ? 'hide-title' : ''">
+                        <div class="flex items-center">
+                            <GoldOutlined />
+                        </div>
+                        <span>Quản lý nguyên liệu</span>
+                    </a-menu-item>
+                    <a-menu-item
+                        key="10"
+                        :class="collapsed ? 'hide-title' : ''"
+                    >
+                        <div class="flex items-center">
+                            <CalculatorOutlined />
+                        </div>
+                        <span>Quản lý đơn vị</span>
+                    </a-menu-item>
+                    <a-menu-item
+                        key="11"
+                        :class="collapsed ? 'hide-title' : ''"
+                    >
+                        <div class="flex items-center">
+                            <FieldNumberOutlined />
+                        </div>
+                        <span>Quản lý nhóm thuộc tính</span>
+                    </a-menu-item>
+                    <a-menu-item
+                        key="12"
+                        :class="collapsed ? 'hide-title' : ''"
+                    >
+                        <div class="flex items-center">
+                            <ExceptionOutlined />
+                        </div>
+                        <span>Quản lý công nợ</span>
+                    </a-menu-item>
+                    <a-menu-item
+                        key="13"
+                        :class="collapsed ? 'hide-title' : ''"
+                    >
+                        <div class="flex items-center">
+                            <ProjectOutlined />
+                        </div>
+                        <span
+                            >Quản lý hóa đơn - Thống kê doanh thu - Báo
+                            giá</span
+                        >
+                    </a-menu-item>
+                    <a-menu-item
+                        key="14"
+                        :class="collapsed ? 'hide-title' : ''"
+                    >
+                        <div class="flex items-center">
+                            <SisternodeOutlined />
+                        </div>
+                        <span>Chức năng khác</span>
                     </a-menu-item>
                 </a-menu>
             </a-layout-sider>
@@ -73,8 +166,7 @@
                 :style="{
                     margin: '24px 16px',
                     padding: '24px',
-                    background: '#fff',
-                    minHeight: '280px',
+                    minHeight: '100vh',
                 }"
             >
                 <slot></slot>
@@ -86,14 +178,25 @@
 import { ref } from "vue";
 import {
     UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
+    HomeOutlined,
+    ShopOutlined,
+    BankOutlined,
+    TeamOutlined,
+    ToolOutlined,
+    AppstoreOutlined,
+    GoldOutlined,
+    FieldNumberOutlined,
+    CalculatorOutlined,
+    ExceptionOutlined,
+    DeliveredProcedureOutlined,
+    ProjectOutlined,
+    SisternodeOutlined,
+    CloudDownloadOutlined,
     MenuUnfoldOutlined,
     MenuFoldOutlined,
     LogoutOutlined,
 } from "@ant-design/icons-vue";
 import { useAuthStore } from "@/stores/auth";
-
 
 const authStore = useAuthStore();
 
@@ -138,6 +241,32 @@ function handleLogout() {
 .site-layout {
     .site-layout-background {
         background: #fff;
+    }
+}
+</style>
+<style lang="scss">
+#gak-cms-dashboard {
+    .ant-layout-sider {
+        .ant-menu-item {
+            height: auto;
+            .ant-menu-title-content {
+                @apply flex gap-2;
+                overflow: visible;
+                white-space: pre-line;
+                line-height: 1.4;
+                padding: 16px 0;
+            }
+
+            &.hide-title {
+                .ant-menu-title-content {
+                    @apply flex gap-2;
+                    overflow: hidden;
+                    white-space: nowrap;
+
+                    padding: 0;
+                }
+            }
+        }
     }
 }
 </style>
--- resources/js/views/pages/private/dashboard/Main.vue
+++ resources/js/views/pages/private/dashboard/Main.vue
@@ -1,16 +1,307 @@
 <template>
     <Page>
-        <div class="w-1/2 m-auto text-center pt-10">
-            <h1 class="text-6xl mb-4 font-bold text-gray-600">Welcome</h1>
-            <h4 class="text-gray-500 text-2xl">{{ new Date() }}</h4>
+        <div class="dasboard-admin space-y-6">
+            <h3 class="text-3xl mt-0">Thống kê chung</h3>
+            <div class="grid grid-cols-2 xl:grid-cols-4 gap-6">
+                <div class="statistic-box">
+                    <div class="title text-2xl font-semibold mb-4">
+                        Tiền bán
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                            >
+                                Hôm nay:
+                            </span>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(100000000) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Hôm qua:</span
+                            >
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng này:</span
+                            >
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng trước:</span
+                            >
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="statistic-box">
+                    <div class="title text-2xl font-semibold mb-4">
+                        Tiền trả
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                            >
+                                Hôm nay:
+                            </span>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Hôm qua:</span
+                            >
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng này:
+                            </span>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng trước:
+                            </span>
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="statistic-box">
+                    <div class="title text-2xl font-semibold mb-4">
+                        Tiền hủy
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Hôm nay:</span
+                            >
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(50000000) }} vnđ</span
+                            >
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                            >
+                                Hôm qua:</span
+                            >
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ</span
+                            >
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng này:</span
+                            >
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }} vnđ</span
+                            >
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng trước:</span
+                            >
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ</span
+                            >
+                        </div>
+                    </div>
+                </div>
+                <div class="statistic-box">
+                    <div class="title text-2xl font-semibold mb-4">
+                        Lượng truy cập
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Hôm nay:</span
+                            >
+                            <span class="number break-all">{{
+                                formatCurrencyNumber(100)
+                            }}</span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Hôm qua:</span
+                            >
+                            <span class="number break-all !text-red-400">{{
+                                formatCurrencyNumber(0)
+                            }}</span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng này:</span
+                            >
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(100) }}
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <span
+                                class="flex-grow-0 flex-shrink-0 "
+                                >Tháng trước:</span
+                            >
+                            <span class="number break-all !text-red-400">{{
+                                formatCurrencyNumber(0)
+                            }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-2 gap-6">
+                <div class="statistic-box">
+                    <div class="title text-2xl font-semibold mb-4">
+                        Hoạt động
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Tổng tiền bán:
+                            </div>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(100000000) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Tổng tiền trả:
+                            </div>
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Tổng tiền hủy:
+                            </div>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }} vnđ
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Hóa đơn bán:
+                            </div>
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }}
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Hóa đơn hủy:
+                            </div>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="statistic-box">
+                    <div class="title text-2xl font-semibold mb-4">
+                        Kho hàng
+                    </div>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Số lượng sản phẩm tồn kho:
+                            </div>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(100000000) }}
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Sản phẩm hết hàng:
+                            </div>
+                            <span class="number break-all !text-red-400"
+                                >{{ formatCurrencyNumber(0) }}
+                            </span>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <div class="flex-grow-0 flex-shrink-0">
+                                Sản phẩm sắp hết hàng:
+                            </div>
+                            <span class="number break-all"
+                                >{{ formatCurrencyNumber(0) }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <footer class="py-5 text-center">
-            <p class="text-gray-400">Hãy dây dựng một cái gì đó thú vị!</p>
-        </footer>
     </Page>
 </template>
 
 <script setup>
 import Page from "@/views/layouts/Page";
 
+const formatCurrencyNumber = (number) => {
+    return number.toLocaleString();
+};
 </script>
+
+<style lang="scss" scoped>
+.statistic-box {
+    background-color: white;
+    padding: 24px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    &.row-flex {
+        flex-direction: row;
+        justify-content: flex-start;
+        color: white;
+        gap: 24px;
+        background-color: #0c6ab0;
+        .number {
+            color: white;
+        }
+    }
+    .number {
+        color: #004b67;
+        font-weight: 600;
+        font-size: 26px;
+    }
+    hr {
+        height: 100%;
+        width: 2px;
+        opacity: 1;
+        color: white;
+        margin: 0;
+        flex-shrink: 0;
+        flex-grow: 0;
+        border: 1px solid;
+    }
+}
+</style>
